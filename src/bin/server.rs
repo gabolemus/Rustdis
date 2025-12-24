171cdba6286e8513427775ffa98fd282496fdd73
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2025-12-24 15:24:44 -0600

    Get HTTP request and display it

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,16 @@
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.expect("Could not get HTTP request"))
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+}
 
 fn main() -> Result<(), std::io::Error> {
     let ip = "127.0.0.1:7878";
@@ -6,9 +18,9 @@ fn main() -> Result<(), std::io::Error> {
     println!("Running server on http://{ip}");
 
     for stream in listener.incoming() {
-        let _stream = stream?;
+        let stream = stream?;
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
 
     Ok(())
